77c8e4c523bfc730355f53eb8da08e22a7f1c29b
DiegoYutaka <diegoyutaka3@gmail.com>
2022-11-15 23:47:42 -0300

    cliente Model
    cliente model

--- PFC_VIBRANIUM_ERP/PFC_VIBRANIUM_ERP/Models/ClienteModel.cs
+++ PFC_VIBRANIUM_ERP/PFC_VIBRANIUM_ERP/Models/ClienteModel.cs
@@ -8,23 +8,70 @@ namespace PFC_VIBRANIUM_ERP.Models
 {
     public class ClienteModel
     {
-        public int id { get; set; }
+        public int idCli { get; set; }
 
         [Required(ErrorMessage = "Favor insira a informação")]
         public string nome { get; set; }
         [Required(ErrorMessage = "Favor insira a informação")]
         [EmailAddress(ErrorMessage = "Favor inserir um email valido")]
-        public string email { get; set; }
+
+        public string pais { get; set; }
+        [Required(ErrorMessage = "Favor informe o país")]
+
+        public string estado { get; set; }
+        [Required(ErrorMessage = "Favor informe o estado")]
+
+
+        public string pessoa { get; set; }
+        [Required(ErrorMessage = "Informe o tipo da pessoa")]
+
+        public string idfederal { get; set; }
+        [Required(ErrorMessage = "Favor informe o CPF ou CNPJ")]
+        
+
+        public string abrev { get; set; }
+        [Required(ErrorMessage = "Favor informe o nome abreviado")]
+
+        //Endereço
+
+        public string descricao { get; set; }
         [Required(ErrorMessage = "Favor insira a informação")]
-        [Phone(ErrorMessage = "Favor inserir um telefone valido")]
+
+        public string cep { get; set; }
+        [Required(ErrorMessage = "Favor informe o CEP")]
+
+        public string numero { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+
+        public string bairro { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+
+        public string cidade { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+
+        public string contato { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+
         public string telefone { get; set; }
         [Required(ErrorMessage = "Favor insira a informação")]
-        public string veiculo { get; set; }
+
+        public string email { get; set; }
         [Required(ErrorMessage = "Favor insira a informação")]
-        public string placa { get; set; }
+
+
+        //Financeiro
+        public string recebimento { get; set; }
         [Required(ErrorMessage = "Favor insira a informação")]
-        public string modelo { get; set; }
+
+        public string banco { get; set; }
         [Required(ErrorMessage = "Favor insira a informação")]
+
+        public string agencia { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+
+        public string conta { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+
         public string cor { get; set; }
     }
 }

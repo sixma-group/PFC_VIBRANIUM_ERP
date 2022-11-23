9f78c9bc5889e296e6df59ef9309537350b05f1a
DiegoYutaka <diegoyutaka3@gmail.com>
2022-11-22 23:48:02 -0300

    Classes abp e acr

---
+++ PFC_VIBRANIUM_ERP/Models/AcrModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFC_VIBRANIUM_ERP.Models
+{
+    public class AcrModel
+    {
+        public int idCli { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public DateTime dtTransacao { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string especie { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string serie { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string titulo { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string valorTitulo { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public DateTime dtVencimento { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public DateTime dtEmissao { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string moeda { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public int parcela { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string status { get; set; }
+
+    }
+}
+

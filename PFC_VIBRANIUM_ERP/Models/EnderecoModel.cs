32f6a14a25e9060a937aebd4104cac1140fc0487
DiegoYutaka <diegoyutaka3@gmail.com>
2022-11-26 13:48:49 -0300

    Endereco Model

---
+++ PFC_VIBRANIUM_ERP/Models/EnderecoModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFC_VIBRANIUM_ERP.Models
+{
+    public class EnderecoModel
+    {
+        public string tipo { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public int id { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public int descricao { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string cep { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string endereco { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public int numero { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string bairro { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string cidade { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string contato { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string telefone { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string email { get; set; }
+        
+    }
+}

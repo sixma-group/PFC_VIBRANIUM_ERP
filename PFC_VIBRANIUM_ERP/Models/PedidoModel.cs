98d60cac6e3ec5b35e10cc5d78f822ea1739616f
DiegoYutaka <diegoyutaka3@gmail.com>
2022-11-28 18:16:17 -0300

    Atualização
    Retirada dos atributos de endereço

--- PFC_VIBRANIUM_ERP/Models/PedidoModel.cs
+++ PFC_VIBRANIUM_ERP/Models/PedidoModel.cs
@@ -33,31 +33,7 @@ namespace PFC_VIBRANIUM_ERP.Models
         [Required(ErrorMessage = "Favor insira a informação")]
 
         public int idForn { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
 
-        //Endereço
-        public string descricao { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
-
-        public string cep { get; set; }
-        [Required(ErrorMessage = "Favor informe o CEP")]
-
-        public int numero { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
-
-        public string bairro { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
-
-        public string cidade { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
-
-        public string contato { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
-
-        public string telefone { get; set; }
-        [Required(ErrorMessage = "Favor insira a informação")]
-
-        public string email { get; set; }
         
     }
 }

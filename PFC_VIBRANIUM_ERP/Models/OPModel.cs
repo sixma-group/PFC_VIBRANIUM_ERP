7eb5a6c3894e4bcf5f67fa1e5c50d1b3165141be
DiegoYutaka <diegoyutaka3@gmail.com>
2022-12-01 19:36:25 -0300

    Ordem de produção
    View e Model

---
+++ PFC_VIBRANIUM_ERP/Models/OPModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFC_VIBRANIUM_ERP.Models
+{
+    public class OPModel
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public int idItem { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public int quantidade { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string status { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string descricao { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public string roteiro { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public DateTime dtEmissao { get; set; }
+        [Required(ErrorMessage = "Favor insira a informação")]
+        public DateTime dtTermino { get; set; }
+    }
+}

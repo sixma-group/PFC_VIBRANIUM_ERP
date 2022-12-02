19e17dea7f73d9efdc7460f154ae872cbc8bc303
VitorSB <vitor.s.barbosa@kroton.com.br>
2022-12-02 02:15:13 -0300

    Implementando a primeira versão das modais de cliente

---
+++ PFC_VIBRANIUM_ERP/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PFC_VIBRANIUM_ERP.Models
+{
+    public class ClienteViewModel
+    {
+        public ClienteModel clienteModel { get; set; }
+        public List<ClienteModel> listaClientes { get; set; }
+    }
+}
